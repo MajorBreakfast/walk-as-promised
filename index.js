9804fb7dd4f226aa3ed670ab9fc19d6807a07bfa
Josef Brandl <mail@josefbrandl.de>
2014-02-18 22:07:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var RSVP = require('rsvp');
+
+var walkSync = require('walk-sync'),
+    walkAsPromised = require('./walk'),
+    walkAsPromisedCStyle = require('./walk-c-style');
+
+
+var folder = __dirname + '/../';
+
+
+// Note: first one always runs slower!
+
+RSVP.resolve()
+.then(runWalkSync)
+.then(runWalkAsPromised)
+.then(runWalkAsPromisedCStyle)
+.then(runWalkSync)
+.then(runWalkAsPromised)
+.then(runWalkAsPromisedCStyle)
+
+function runWalkSync() {
+  var start = Date.now();
+  var result = walkSync(folder, '/');
+  console.log('Sync took ' + (Date.now() - start) + ' ms');
+}
+
+function runWalkAsPromised() {
+  var start = Date.now()
+  return walkAsPromised(folder, '/')
+  .then(function(result) {
+    console.log('walkAsPromised took ' + (Date.now() - start) + ' ms');
+  });
+}
+
+function runWalkAsPromisedCStyle() {
+  var start = Date.now()
+  return walkAsPromisedCStyle(folder, '/')
+  .then(function(result) {
+    console.log('walkAsPromisedCStyle took ' + (Date.now() - start) + ' ms');
+  });
+}
---
+++ walk-c-style.js
@@ -0,0 +1,49 @@
+var fs = require('fs'),
+    RSVP = require('rsvp'),
+    stat = RSVP.denodeify(fs.stat),
+    readdir = RSVP.denodeify(fs.readdir),
+    Promise = RSVP.Promise;
+
+module.exports = walk;
+function walk(baseDir, relativePath) {
+  return readdir(baseDir + '/' + relativePath)
+    .then(function(entries) {
+      var files = new Array(entries.length),
+          filesCount = 0,
+          folderPromises = new Array(entries.length),
+          folderPromisesCount = 0;
+
+      var statPromises = entries.map(function(entry) {
+        return stat(baseDir + '/' + relativePath + entry);
+      });
+
+      return RSVP.all(statPromises)
+        .then(function(stati) {
+          for (var i = 0; i < entries.length; i++) {
+            if (stati[i].isDirectory()) {
+              folderPromises[folderPromisesCount++] = walk(baseDir, relativePath + entries[i] + '/');
+            } else {
+              files[filesCount++] = entries[i];
+            }
+          };
+
+          return RSVP.all(folderPromises);
+        })
+        .then(function(subentries) {
+
+          // How long is the result array?
+          var sum = 1 + filesCount;
+          for (var a = 0; a < folderPromisesCount; a++) { sum += subentries[a].length; };
+          // Create an array of the correct size aand populate it
+          var offset = 0;
+          var output = new Array(sum);
+          output[offset++] = relativePath; // Root folder
+          for (var b = 0; b < filesCount; b++) { output[offset++] = files[b]; }; // Files
+          for (var c = 0; c < folderPromisesCount; c++) { // Entries of subfolders
+            for (d = 0; d < subentries[c].length; d++) { output[offset++] = subentries[c][d]; };
+          };
+
+          return output;
+        });
+    });
+};
