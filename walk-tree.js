85fa6644214732af35ff3e7ae799dbed78cacd33
Josef Brandl <mail@josefbrandl.de>
2014-04-14 01:09:52 +0200

    Remove slash properly

--- walk-tree.js
+++ walk-tree.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs'),
     RSVP = require('rsvp');
 
@@ -5,15 +7,15 @@ module.exports = walk;
 function walk(dir, options) {
 
   // Default options
-  options = options || {}
+  options = options || {};
   var processDirectory = options.processDirectory ||
     function(baseDir, relativePath, stat, entries, callback) {
-      callback(null, Array.prototype.concat.apply([relativePath + '/'], entries));
-    }
+      callback(null, Array.prototype.concat.apply([relativePath], entries));
+    };
   var processFile = options.processFile ||
     function(baseDir, relativePath, stat, callback) {
       callback(null, [relativePath]);
-    }
+    };
 
   var fsReaddir = options.sync ? fsReaddirSync : fs.readdir;
   var fsStat = options.sync ? fsStatSync : fs.stat;
@@ -23,11 +25,11 @@ function walk(dir, options) {
 
   function fly(relativePath, j, callback) {
     // Note: j is just an index that gets passed through (Needed for recursive calling)
-
     fsStat(dir + '/' + relativePath, function(err, stat) {
       if (err) { callback(err); return; }
 
       if (stat.isDirectory()) { // Directory
+        if (relativePath !== '') { relativePath += '/'; }
 
         fsReaddir(dir + '/' + relativePath, function(err, entries) {
           if (err) { callback(err); return; }
@@ -35,7 +37,7 @@ function walk(dir, options) {
           if (entries.length > 0) { // Full directory
             var entriesLeft = entries.length;
             for (var i = 0; i < entries.length; i++) {
-              fly(relativePath + entries[i] + '/', i, function(err, subentries, i) {
+              fly(relativePath + entries[i], i, function(err, subentries, i) {
                 entries[i] = subentries;
                 entriesLeft -= 1;
                 if (entriesLeft === 0) {
@@ -44,14 +46,14 @@ function walk(dir, options) {
                   });
                 }
               });
-            };
+            }
           } else { // Empty directory
             processDirectory(dir, relativePath, stat, entries, function(err, result) {
               if (err) { callback(err); } else { callback(null, result, j); }
             });
           }
 
-        })
+        });
 
       } else if (stat.isFile()) { // File
         processFile(dir, relativePath, stat, function(err, result) {
@@ -60,11 +62,11 @@ function walk(dir, options) {
       }
 
     });
-  };
+  }
 
 
-  return before().then(function() { return RSVP.denodeify(fly)('', null); }).then(after);
-};
+  return before().then(RSVP.denodeify(fly).bind(null, '')).then(after);
+}
 
 function fsReaddirSync(file, cb) { cb(null, fs.readdirSync(file)); }
-function fsStatSync(file, cb) { cb(null, fs.statSync(file)) }
+function fsStatSync(file, cb) { cb(null, fs.statSync(file)); }
